49ce9008d4b135b3c6422c8cf24df16deda49eaa
chenj <chenj7498@gmail.com>
2023-11-29 17:28:42 +0900

    프로젝트 기본 세팅

---
+++ backend/CleanSnowTown/src/main/java/com/project/cleansnowtown/CleanSnowTownApplication.java
@@ -0,0 +1,13 @@
+package com.project.cleansnowtown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanSnowTownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanSnowTownApplication.class, args);
+    }
+
+}
---
+++ backend/CleanSnowTown/src/test/java/com/project/cleansnowtown/CleanSnowTownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cleansnowtown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanSnowTownApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
